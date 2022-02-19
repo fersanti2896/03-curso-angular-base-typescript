6c0e7018d952da45cb55eb6a836b31d487f45f34
Fernando Nicolas <fersanti2896@gmail.com>
2022-02-18 22:47:31 -0600

    Agregando ejercicio de Desestructuracion de Objetos

--- src/ejercicios/05-deses-obj.ts
+++ src/ejercicios/05-deses-obj.ts
@@ -1,2 +1,31 @@
 
-/* Desestructuiracion de Arreglos */
+/* Desestructuiracion de Objetos */
+interface Reproductor {
+    volumen: number;
+    segundo: number;
+    cancion: string;
+    detalles: Detalles;
+}
+
+interface Detalles {
+    autor: string;
+    anio: number;
+}
+
+const reproductor: Reproductor = {
+    volumen: 90,
+    segundo: 36,
+    cancion: 'Mi media sandía',
+    detalles: {
+        autor: 'Raúl Ornelas',
+        anio: 2013
+    }
+}
+
+const { volumen, segundo, cancion, detalles: { autor, anio } } = reproductor;
+ 
+console.log(`El volumen actual: ${volumen}`);
+console.log(`El segundo actual: ${segundo}`);
+console.log(`La canción es: ${cancion}`);
+console.log(`El cantante es: ${autor}`);
+console.log(`El año de la canción es: ${anio}`);
